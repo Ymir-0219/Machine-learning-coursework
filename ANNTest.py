109f0e04987b7112fc2af84af7a0a4d89ecb9b00
Ymir-0219 <55649127+Ymir-0219@users.noreply.github.com>
2022-01-10 22:15:42 +0800

    Add files via upload

--- ANNTest.py
+++ ANNTest.py
@@ -14,11 +14,11 @@ plt.rcParams['font.sans-serif']=['SimHei']
 plt.rcParams['axes.unicode_minus']=False
 
 alpha=0.1
-times=150
+times=1000
 kf=KFold(n_splits=5)
 Output_Layer_num=4#输出层的神经元数量即类别数
-Hidden_Layer_num=5#隐藏层的神经元数量
-Input_layer_num=3#输入层的神经元数量即输入的特征量
+Hidden_Layer_num=3#隐藏层的神经元数量
+Input_layer_num=2#输入层的神经元数量即输入的特征量
 
 def normalized(X):
     X_min,X_max=np.min(X),np.max(X)
@@ -36,82 +36,98 @@ def out_predict(X):
         out[i]=X[i].tolist().index(max(X[i]))
     return out
 
-def sigmoid(X):
-    return 1/(1+np.exp(-X))
+def sigmoid(X,num):
+    Y=np.zeros((len(X),num))
+    for i in range(len(X)):
+        for j in range(num):
+            if X[i][j]>=0:
+                Y[i][j]=1/(1+np.exp(-X[i][j]))
+            else :
+                Y[i][j]=np.exp(X[i][j])/(1+np.exp(X[i][j]))
+    return Y
 
 def softmax(X):
-    Out=np.zeros((6,4))
     for i in range(len(X)):
         val_max=max(X[i])
-        Out[i]=np.exp(X[i]-val_max) / np.sum(np.exp(X[i]-val_max))
-    return Out
+        X[i]=np.exp(X[i]-val_max) / np.sum(np.exp(X[i]-val_max))
+        #X[i]=np.exp(X[i]) / np.sum(np.exp(X[i]))
+    return X
 
 def get_Mid_input(theta,x):
     return np.dot(x,theta)
 
-def get_Hidden_Layer(X):
-    return sigmoid(X)
+def get_Hidden_Layer(X,gama):
+    return sigmoid(X+gama,Hidden_Layer_num)
 
-def get_Output_Layer(Output_layer_Input):
-    #print(Output_layer_Input)
-    return softmax(Output_layer_Input)
+def get_Output_Layer(Output_layer_Input,theta):
+    return sigmoid(Output_layer_Input+theta,Output_Layer_num)
+    #return softmax(X+theta)
 
 def LMS(is_index_train,Output):
-    return 0.5*np.sum((is_index_train-Output)**2)
+    return 0.5*np.sum((Output-is_index_train)**2)
 
 def CE(is_index_train,Output):
     return -np.sum(is_index_train*np.log(Output))
 
 def get_error_OutputLayer(is_index_train,Output):
-    #print((Output-is_index_train)*Output*(1-Output))
     return (Output-is_index_train)*Output*(1-Output)
 
-def get_error_HiddenLayer(is_index_train, Output,W,Hidden_Layer):
-    error_OutputLayer=get_error_OutputLayer(is_index_train, Output)   
+def get_error_HiddenLayer(error_OutputLayer,W,Hidden_Layer):   
     return np.dot(error_OutputLayer,W.T)*Hidden_Layer*(1-Hidden_Layer)
 
-def upload_W(W,is_index_train,Output,Hidden_Layer):
-    error_OutputLayer=get_error_OutputLayer(is_index_train, Output)   
-    W-=alpha*(np.dot(Hidden_Layer.T,error_OutputLayer))
-    return W
+def upload(W,V,theta,gama,Output,is_index_train,Hidden_Layer,Input,error_OutputLayer,error_HiddenLayer):
     
-def upload_V(V,is_index_train, Output, W, Hidden_Layer,Input):
-    error_HiddenLayer=get_error_HiddenLayer(is_index_train, Output, W, Hidden_Layer)
+    W-=alpha*(np.dot(Hidden_Layer.T,error_OutputLayer))
     V-=alpha*(np.dot(Input.T,error_HiddenLayer))
-    return V
+    gama-=alpha*np.sum(error_HiddenLayer,axis=0)
+    theta-=alpha*np.sum(error_OutputLayer,axis=0)
+    return W,V,theta,gama
+    
 
 if __name__ == '__main__':
 
-    data_x1_train=np.array([1,2,3,4,5,6])
-    data_x2_train=np.array([2,1,4,5,3,6])
-    data_index_train=np.array([0,2,1,0,3,1]).T 
+    data_x1_train=np.array([1,1,6,3,4,6,4.5])
+    data_x2_train=np.array([2,1,4,5,3,6,3.2])
+    data_index_train=np.array([0,2,1,0,3,1,3]).T 
     #构造训练集的输入
     x_show = np.stack((data_x1_train.flat, data_x2_train.flat), axis=1)
-    one=np.ones((len(x_show),1))
-    Input_train=np.c_[x_show,one]
-    #训练集上的误差
+    Input_train=x_show
     errorList=[]
     #测试集的正确率
-    AccuracyList=[]   
+    AccuracyList=[]
+    
     rd = np.random.RandomState(12) 
     #输入层权重
     #V=np.ones((Input_layer_num,Hidden_Layer_num-1))
-    V=rd.normal(0,4,(Input_layer_num,Hidden_Layer_num-1))
+    V=rd.normal(0,0,(Input_layer_num,Hidden_Layer_num))
+    #输入层的偏移
+    gama=rd.normal(0,0,(1,Hidden_Layer_num))
+    print(gama)
     #输出层权重
     #W=np.ones((Hidden_Layer_num,Output_Layer_num))
-    W=rd.normal(0,4,(Hidden_Layer_num,Output_Layer_num))
+    W=rd.normal(0,0,(Hidden_Layer_num,Output_Layer_num))
+    print(W,V)
+    #输出层的偏移
+    theta=rd.normal(0,0,(1,Output_Layer_num))
     #得到训练集的独热码
     is_index_train=get_OneHot(data_index_train)
+    #得到训练集的独热码
+    is_index_train=get_OneHot(data_index_train)
+    for i in range(times):
+        Hidden_Layer_Input=get_Mid_input(V, Input_train)
+        Hidden_Layer=get_Hidden_Layer(Hidden_Layer_Input,gama)
+        Output_layer_Input=get_Mid_input(W, Hidden_Layer)
+        Output=get_Output_Layer(Output_layer_Input,theta)
+        error_OutputLayer=get_error_OutputLayer(is_index_train, Output)
+        error_HiddenLayer=get_error_HiddenLayer(error_OutputLayer,W, Hidden_Layer)
+        #AccuracyList.append(get_Acc(data_index_test,Input_test,W,V,gama,theta))
+        #print(np.dot(Hidden_Layer.T,error_OutputLayer),np.dot(Input_train.T,error_HiddenLayer),np.sum(error_HiddenLayer,axis=0),np.sum(error_OutputLayer,axis=0))
+        W_change=np.dot(Hidden_Layer.T,error_OutputLayer)
+        V_change=np.dot(Input_train.T,error_HiddenLayer)
+        gama_change=np.sum(error_HiddenLayer,axis=0)
+        theta_change=np.sum(error_OutputLayer,axis=0)
+        W,V,theta,gama=upload(W, V, theta, gama, Output, is_index_train, Hidden_Layer, Input_train,error_OutputLayer,error_HiddenLayer)
+        errorList.append(LMS(is_index_train, Output))
     
-    Hidden_Layer_Input=get_Mid_input(V, Input_train)
-    Hidden_Layer_mid=get_Hidden_Layer(Hidden_Layer_Input)
-    one=np.ones((len(Hidden_Layer_mid),1))
-    Hidden_Layer=np.c_[Hidden_Layer_mid,one]
-    Output_layer_Input=get_Mid_input(W, Hidden_Layer)
-    Output=get_Output_Layer(Output_layer_Input)
-    W=upload_W(W, is_index_train, Output, Hidden_Layer)
-    V=upload_V(V, is_index_train, Output, W, Hidden_Layer, Input_train)
-    
-    errorList.append(LMS(is_index_train, Output))
-    print(Output)
+
     
---
+++ Perceptron.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 23 11:06:45 2021
+
+@author: syr11
+"""
+import pandas as pd  
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from sklearn.model_selection import KFold
+
+plt.rcParams['font.sans-serif']=['SimHei']
+plt.rcParams['axes.unicode_minus']=False
+
+times=100
+kf=KFold(n_splits=5)
+K=3#改数字
+
+def readData():
+    data=pd.read_csv("GMM3.csv")#该文件夹名字
+    index=data.iloc[:,0]
+    x1=data.iloc[:,1]
+    x2=data.iloc[:,2]   
+    return index,x1,x2
+
+def normalized(X):
+    return (X-X.min)/(X.max-X.min)
+
+def GMM(X):
+    return X.tolist().index(max(X))
+
+def get_error(theta,X,X_index):
+    error=0
+    for i in range(len(X)):
+        error+=np.dot(theta[int(GMM(np.dot(theta,X[i])))],X[i])-np.dot(theta[int(X_index[i])],X[i])
+    return error
+
+def upload_theta(theta,X,X_index,alpha):
+    for j in range(len(X)):
+        if GMM(np.dot(theta,X[j]))!=X_index[j]:
+            theta[GMM(np.dot(theta,X[j]))]-=alpha*X[j]
+            theta[int(X_index[j])]+=alpha*X[j]
+    return theta
+
+def upload_Accuracy(theta,X,X_index):
+    index_Acc=[]
+    for i in range(0,len(X)):
+        index_Acc.append(GMM(np.dot(theta,X[i])))
+    np.array(index_Acc)
+    Accuracy=(np.sum(index_Acc==X_index))/len(X)
+    return Accuracy,index_Acc
+if __name__ == '__main__':
+    data_index,data_x1,data_x2=readData()
+    for train_index, test_index in kf.split(data_index): 
+        data_index_train, data_index_test = data_index[train_index], data_index[test_index]
+        data_x1_train, data_x1_test =data_x1[train_index], data_x1[test_index]
+        data_x2_train,data_x2_test = data_x2[train_index], data_x2[test_index]
+    data_x1_train=np.array(data_x1_train)
+    data_x2_train=np.array(data_x2_train)
+    data_x1_test=np.array(data_x1_test)
+    data_x2_test=np.array(data_x2_test)
+    data_index_test=np.array(data_index_test)    
+    x_show = np.stack((data_x1_train.flat, data_x2_train.flat), axis=1)
+    #x_show=normalized(x_show)
+    one=np.ones((len(x_show),1))
+    X=np.c_[x_show,one]
+    SGD_X=np.c_[x_show,data_index_train]
+    x_show = np.stack((data_x1_test.flat, data_x2_test.flat), axis=1)
+    #x_show=normalized(x_show)
+    one=np.ones((len(x_show),1))
+    X_test=np.c_[x_show,one]
+    errorList=[]
+    AccuracyList=[]
+    theta=np.zeros((K,3))
+    plt.ion()
+    alpha=0.1
+    fig = plt.figure(figsize=(17, 6))
+    for i in range(times):
+       # mini_X=random.sample(list(SGD_X),500)
+        #mini_X=np.array(mini_X)
+        #mini_X_index=mini_X[:,2]
+        #np.delete(mini_X,2,axis=1)
+        #theta=upload_theta(theta, mini_X, mini_X_index)
+        #errorList.append(get_error(theta, mini_X, mini_X_index))
+        
+        
+        theta=upload_theta(theta, X, data_index_train,alpha)
+        alpha*=0.9
+        errorList.append(get_error(theta, X, data_index_train))
+        Accuracy,index_Acc=upload_Accuracy(theta,X_test,data_index_test)
+        AccuracyList.append(Accuracy)
+        ax1 = fig.add_subplot(131)
+        plt.sca(ax1)
+        plt.plot([x for x in range(len(errorList))], errorList, c='blue', label='误差迭代曲线')
+        
+        ax2 = fig.add_subplot(132)
+        plt.sca(ax2)       
+        plt.plot([x for x in range(len(AccuracyList))], AccuracyList, c='red', label='预测正确率')
+        
+        ax3 = fig.add_subplot(133)
+        plt.sca(ax3)
+        N, M = 200,200 
+        x1_min, x2_min = min(data_x1_train)-0.5,min(data_x2_train)-0.5
+        x1_max, x2_max =max(data_x1_train)+0.5,max(data_x2_train)+0.5
+        t1 = np.linspace(x1_min, x1_max, N)
+        t2 = np.linspace(x2_min, x2_max, M)
+        x1, x2 = np.meshgrid(t1, t2)
+        x_show = np.stack((x1.flat, x2.flat), axis=1)
+        one=np.ones((len(x_show),1))
+        x_show=np.c_[x_show,one]
+        y_predict=[]
+        for i in range(0,len(x_show)):
+            y_predict.append(GMM(np.dot(theta,x_show[i])))
+        y_predict=np.array(y_predict)
+        mpl.rcParams['font.sans-serif'] = ['SimHei']
+        mpl.rcParams['axes.unicode_minus'] = False
+        cm_light = mpl.colors.ListedColormap(['#A0FFA0', '#FFA0A0','#A0A0FF','#FF16D5','#2CAAE6','#392EE6','#E9F07C','#608F0E'])
+        cm_dark = mpl.colors.ListedColormap(['#436B45', '#FF45A1','#A7AAFF','#E859FF','#9A9882','#3AC5F1','#917261','#FFD648'])
+        #将对应数量的值放入
+        #cm_light=['#A0FFA0', '#FFA0A0','#A0A0FF','#FF16D5','#2CAAE6','#392EE6','#E9F07C','#608F0E']
+        #cm_dark['#436B45', '#FF45A1','#A7AAFF','#E859FF','#9A9882','#3AC5F1','#917261','#FFD648']
+        plt.xlim(x1_min, x1_max)
+        plt.ylim(x2_min, x2_max)
+        plt.pcolormesh(x1, x2, y_predict.reshape(x1.shape), cmap=cm_light)
+        plt.scatter(data_x1_train,data_x2_train,s=2,c=data_index_train,cmap=cm_dark)
+        plt.pause(0.01)
+    print(theta)
+    plt.ioff()
+    plt.show()
